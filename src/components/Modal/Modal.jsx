a59eca700f232fe618cf643a90dba6e8110ceef5
HimaGirish314 <Himagirish.bandi@gmail.com>
2023-01-09 16:17:37 +0530

    model feature is added

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Modal.css'
+
+const Modal = ({modalData,setModal}) => {
+  const {flag,name,capital,region,population,timezones}=modalData
+  return (
+    <div className='modal-card'>
+        <div className="country-image"><img src={flag} alt="country-flag" /></div>
+        <div className="country-details">
+            <h3>Name:<span className='value-name'>{name}</span></h3>
+            <h3>Capital:<span className='value-name'>{capital}</span></h3>
+            <h3>Region:<span className='value-name'>{region}</span></h3>
+            <h3>Population:<span className='value-name'>{population}</span></h3>
+            <button onClick={()=>setModal(false)}>Close</button>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Modal
